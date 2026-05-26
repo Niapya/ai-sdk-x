7cc28d3e70487b2a47a16600352d54c7415a0f91
niapya <hackhtu@proton.me>
2026-05-26 13:29:59 +0800

    feat: add list method to KVStorage interface

--- packages/ai-sdk-x/src/types/storage/kv.ts
+++ packages/ai-sdk-x/src/types/storage/kv.ts
@@ -1,6 +1,7 @@
 import type { Awaitable } from "..";
 
 export interface KVStorage {
+	list: (prefix?: string, limit?: number) => Awaitable<string[]>;
 	get: (key: string) => Awaitable<null | string | undefined>;
 	set: (key: string, value: string, ttl?: number) => Awaitable<void>;
 	delete: (key: string) => Awaitable<void>;
